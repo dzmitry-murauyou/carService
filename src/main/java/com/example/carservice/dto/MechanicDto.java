3abc309d035e67ab1589987f925e68df24691e87
Dzmitry <dz.murauyou@gmail.com>
2026-04-06 23:44:16 +0300

    Lab4

--- src/main/java/com/example/carservice/dto/MechanicDto.java
+++ src/main/java/com/example/carservice/dto/MechanicDto.java
@@ -17,21 +17,17 @@ import lombok.NoArgsConstructor;
 @Schema(description = "DTO with mechanic information")
 public class MechanicDto {
 
-  @Schema(description = "Unique mechanic identifier", example = "1")
   private Long id;
 
   @NotBlank(message = "Mechanic first name is required")
   @Size(max = 40, message = "Mechanic first name must contain no more than 40 characters")
-  @Schema(description = "First name of mechanic", example = "Petr")
   private String firstName;
 
   @NotBlank(message = "Mechanic last name is required")
   @Size(max = 40, message = "Mechanic last name must contain no more than 40 characters")
-  @Schema(description = "Last name of mechanic", example = "Petrov")
   private String lastName;
 
   @Size(max = 13, message = "Mechanic phone number must contain no more than 13 characters")
-  @Schema(description = "Contact phone number", example = "+375291234567")
   private String phone;
 
   @Schema(description = "Date when mechanic was hired", example = "2023-01-10")
